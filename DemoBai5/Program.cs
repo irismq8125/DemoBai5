8ce54952d63b854f95f8605c88cbe71bb2a907e0
irismq8125 <mauquy123@gmail.com>
2023-08-18 20:57:55 +0700

    Update code

--- DemoBai5/Program.cs
+++ DemoBai5/Program.cs
@@ -43,57 +43,57 @@ namespace DemoBai5
             Console.WriteLine("Phan tu dau tien: {0}", arrays[0]);
 
 
-            //tinh tong mang voi cac so chia het cho 2
-            //Console.WriteLine("Do dai cua mang Arrays la: {0}", arrays.Length);
-            //int s = 0;
-            //int k = 0;
-            //int t = 0;
-            //int p = 0;
-            ////s = s + arrays[0];
-            ////s = s + arrays[1];
-            ////s = s + arrays[2];
-            ////s = s + arrays[3];
-            ////s = s + arrays[4];
-            ////s = s + arrays[5];
-            ////s = s + arrays[6];
-            ////s = s + arrays[7];
-            ////s = s + arrays[8];
-            ////s = s + arrays[9];
-            ////s = s + arrays[10];
-            ////Console.WriteLine("Tong mang la: {0}", s);
+            tinh tong mang voi cac so chia het cho 2
+            Console.WriteLine("Do dai cua mang Arrays la: {0}", arrays.Length);
+            int s = 0;
+            int k = 0;
+            int t = 0;
+            int p = 0;
+            //s = s + arrays[0];
+            //s = s + arrays[1];
+            //s = s + arrays[2];
+            //s = s + arrays[3];
+            //s = s + arrays[4];
+            //s = s + arrays[5];
+            //s = s + arrays[6];
+            //s = s + arrays[7];
+            //s = s + arrays[8];
+            //s = s + arrays[9];
+            //s = s + arrays[10];
+            //Console.WriteLine("Tong mang la: {0}", s);
 
-            //for (int i = 0; i < arrays.Length; i++)
-            //{
-            //    if (arrays[i] % 2 == 0)
-            //    {
-            //        k = k + arrays[i];
-            //    }
-            //}
-            //Console.WriteLine("Tong mang voi for la: {0}", k);
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                if (arrays[i] % 2 == 0)
+                {
+                    k = k + arrays[i];
+                }
+            }
+            Console.WriteLine("Tong mang voi for la: {0}", k);
 
-            //foreach (var item in arrays) //item == arrays[i]
-            //{
-            //    if(item % 2 == 0)
-            //    {
-            //        t = t + item;
-            //    }
-            //}
-            //Console.WriteLine("Tong mang voi foreach la: {0}", t);
+            foreach (var item in arrays) //item == arrays[i]
+            {
+                if (item % 2 == 0)
+                {
+                    t = t + item;
+                }
+            }
+            Console.WriteLine("Tong mang voi foreach la: {0}", t);
 
-            //p = arrays.Where(o => o % 2 == 0).Sum();
-            //Console.WriteLine("Tong mang voi Sum la: {0}", p);
+            p = arrays.Where(o => o % 2 == 0).Sum();
+            Console.WriteLine("Tong mang voi Sum la: {0}", p);
 
-            //////tim so lon nhat trong mang
-            ////int max = arrays.Max();
-            ////Console.WriteLine("So lon nhat la: {0}", max);
+            ////tim so lon nhat trong mang
+            //int max = arrays.Max();
+            //Console.WriteLine("So lon nhat la: {0}", max);
 
-            //////tim so nho nhat trong mang
-            ////int min = arrays.Min();
-            ////Console.WriteLine("So lon nhat la: {0}", min);
+            ////tim so nho nhat trong mang
+            //int min = arrays.Min();
+            //Console.WriteLine("So lon nhat la: {0}", min);
 
-            //////tim so trung binh trong mang
-            ////double a = arrays.Average();
-            ////Console.WriteLine("Trung binh la: {0}", a);
+            ////tim so trung binh trong mang
+            //double a = arrays.Average();
+            //Console.WriteLine("Trung binh la: {0}", a);
             Console.ReadKey();
         }
     }
